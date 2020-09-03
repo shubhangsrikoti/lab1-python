164bc650b7f4d66a5496629e01c65cc5fbb5f1f5
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-03 21:49:16 +0000

    removed spaces

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ unit = str(input("Enter unit in F/f or C/c: "))
 if unit == "F" or unit == "f":
   print("Converting Fahrenheit to Celsius.")
   newCelsius = ((temp - 32) * 5)/9
-  print(f"{temp}° in Fahrenheit is equivalent to {newCelsius}°  Celsius.")
+  print(f"{temp}° Fahrenheit is equivalent to {newCelsius}° Celsius.")
 elif unit == "C" or unit == "c":
   print("Converting Celsius to Fahrenheit.")
   newFahrenheit = ((temp * 9)/5) + 32
-  print(f"{temp}° in Celsius is equivalent to {newFahrenheit}° Fahrenheit.")
+  print(f"{temp}° Celsius is equivalent to {newFahrenheit}° Fahrenheit.")
