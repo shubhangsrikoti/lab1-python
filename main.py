6fe71203f84cb4b11138a4bcec0d48a0bcc8e5d7
Shubhang Srikoti <70000106+shubhangsrikoti@users.noreply.github.com>
2020-09-03 22:36:56 +0000

    added else statement for bad input

--- main.py
+++ main.py
@@ -14,3 +14,5 @@ elif unit == "C" or unit == "c":
   ##print("Converting Celsius to Fahrenheit.")
   newFahrenheit = (temp * 1.8) + 32
   print(f"{temp}° in Celsius is equivalent to {newFahrenheit}° Fahrenheit.")
+else:
+  print(f"Invalid unit ({unit}).")
